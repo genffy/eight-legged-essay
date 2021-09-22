2a97e25d9a6d881bcd4cbc0032de92cf051694d0
genffy <lee.genffy@gmail.com>
2021-09-22 15:48:53 +0800

    update

--- src/leetcode/generate-parentheses.js
+++ src/leetcode/generate-parentheses.js
@@ -2,15 +2,98 @@
  * @param {number} n
  * @return {string[]}
  */
-var generateParenthesis = function (n) {
+ var generateParenthesis = function (n) {
   if (n < 1 || n > 8) return [];
-  const chars = ('('.repeat(n) + ')'.repeat(n)).split('');
-  const _chars = [...new Array(2 * n).keys()];
-  console.log('_chars', _chars);
-  // 计算所有组合
-  const arr = func(chars);
-  return arr.filter(v => valid(v));
+  let arr = [];
+  // const chars = ("(".repeat(n) + ")".repeat(n)).split("");
+  // // 计算所有组合
+  // arr = func(chars);
+  // arr = funcv2(chars);
+  backtrack(arr, [], 0, 0, n);
+  return arr;
 };
+
+function backtrack(ans, cur, open, close, max) {
+  if (cur.length == max * 2) {
+      ans.push(cur.join(""));
+      return;
+  }
+  if (open < max) {
+      cur.push('(');
+      backtrack(ans, cur, open + 1, close, max);
+      cur.pop();
+  }
+  if (close < open) {
+      cur.push(')');
+      backtrack(ans, cur, open, close + 1, max);
+      cur.pop();
+  }
+}
+
+// 全排列v1
+function func(arr) {
+  let len = arr.length
+  let res = [] // 所有排列结果
+  /**
+   * 【全排列算法】
+   * 说明：arrange用来对 arr 中的元素进行排列组合，将排列好的各个结果存在新数组中
+   * @param tempArr：排列好的元素
+   * @param leftArr：待排列元素
+   */
+  let arrange = (tempArr, leftArr) => {
+    if (tempArr.length === len) { // 这里就是递归结束的地方
+      // res.push(tempArr) // 得到全排列的每个元素都是数组
+      const str = tempArr.join("");
+      if(!res.includes(str) && valid(str)){
+        res.push(str) // 得到全排列的每个元素都是字符串
+      }
+    } else {
+      leftArr.forEach((item, index) => {
+        let temp = [].concat(leftArr)
+        temp.splice(index, 1)
+        // 此时，第一个参数是当前分离出的元素所在数组；第二个参数temp是传入的leftArr去掉第一个后的结果
+        arrange(tempArr.concat(item), temp) // 这里使用了递归
+      })
+    }
+  }
+  arrange([], arr)
+  return res;
+}
+// 全排列
+function funcv2(arr) {
+  let len = arr.length
+  let res = [] // 所有排列结果
+  const used = new Array(len).fill(false);
+  /**
+   * 【全排列算法】
+   * 说明：arrange用来对 arr 中的元素进行排列组合，将排列好的各个结果存在新数组中
+   * @param tempArr：排列好的元素
+   * @param leftArr：待排列元素
+   */
+  let arrange = (collect, len, deep, path, used) => {
+    if (deep === len) {
+      const str = path.join("");
+      if(!res.includes(str) && valid(str)){
+        res.push(str)
+      }
+    } else {
+      for (let i = 0; i < len; i++) {
+        if(!used[i]){
+          path.push(collect[i]);
+          used[i] = true;
+
+          arrange(collect, len, deep+1, path, used)
+          
+          used[i] = false;
+          path.pop();
+        }      
+      }
+    }
+  }
+  arrange(arr, len, 0, [], [], used)
+  return res;
+}
+// 验证是否合法
 function valid (s) {
   const len = s.length;
   const stack = [];
@@ -29,29 +112,5 @@ function valid (s) {
   }
   return stack.length == 0;
 }
-let func = (arr) => {
-  let len = arr.length
-  let res = [] // 所有排列结果
-  /**
-   * 【全排列算法】
-   * 说明：arrange用来对 arr 中的元素进行排列组合，将排列好的各个结果存在新数组中
-   * @param tempArr：排列好的元素
-   * @param leftArr：待排列元素
-   */
-  let arrange = (tempArr, leftArr) => {
-    if (tempArr.length === len) { // 这里就是递归结束的地方
-      // res.push(tempArr) // 得到全排列的每个元素都是数组
-      res.push(tempArr.join('')) // 得到全排列的每个元素都是字符串
-    } else {
-      leftArr.forEach((item, index) => {
-        let temp = [].concat(leftArr)
-        temp.splice(index, 1)
-        // 此时，第一个参数是当前分离出的元素所在数组；第二个参数temp是传入的leftArr去掉第一个后的结果
-        arrange(tempArr.concat(item), temp) // 这里使用了递归
-      })
-    }
-  }
-  arrange([], arr)
-  return Array.from([...new Set(res)])
-}
-console.log(generateParenthesis(3));
+// 测试
+// console.log(generateParenthesis(6));
