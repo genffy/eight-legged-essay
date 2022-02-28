c168530fb1705cff37fff85282973629850f3563
genffy <genffyl@gmail.com>
2022-02-28 10:22:38 +0800

    feat: update find-number-in-2d-array

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 // import '@/leetcode/excel-pos';
 // import '@/leetcode/binary-balance';
 // import '@/leetcode/binary-path';
-import '@/leetcode/sqrt';
+// import '@/leetcode/sqrt';
 // import '@/leetcode/top-k'
 // import '@/leetcode/task-scheduler'
 // import '@/leetcode/search-range'
 // import '@/leetcode/generate-parentheses'
 // import '@/leetcode/merge-two-sorted-lists'
+import '@/leetcode/find-number-in-2d-array'
 // import '@/basic/sort'
 // import '@/basic/eat-man'
 // import '@/handwrite/condition-join'
